b72f38df5e82025b69aa130d1773ad96311c4ea4
vishalpandita <vishalpandita@doshaheen.com>
2023-03-08 18:18:09 +0530

    User Login API

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,6 @@ import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AssociationService } from '../association/association.service';
-//import { Country } from './entities/countries.entity';
 import { User } from './entities/user.entity';
 import { loginDto } from './dto/login-user.dto';
 import { Association } from 'src/association/entities/association.entity';
@@ -26,20 +25,21 @@ export class UsersService {
     return this.userRepository.findOneBy({ username });
   }
 
-  async fetchUserDetails(username: string): Promise<User> {
-    return await this.userRepository.createQueryBuilder('u')
-      .select('u.id', 'userId')
-      .addSelect('u.username', 'userName')
-      .addSelect('u.email', 'userEmail')
-      .addSelect('a.association_name', 'associationName')
-      .addSelect('a.id', 'associationId')
-      .addSelect('p.province_name', 'proviencName')
-      .addSelect('c.country_name', 'countryName')
-      .leftJoin(Association, 'a', 'u.associationId = a.id')
-      .leftJoin(Province, 'p', 'a.provinceId = p.id')
-      .leftJoin(Country, 'c', 'p.countryId = c.id')
+  async fetchUserDetails(username: string){
+    const userData = await this.userRepository.createQueryBuilder("u")
+      .select("u.id", "userId")
+      .addSelect("u.username", "userName")
+      .addSelect("u.email", "userEmail")
+      .addSelect("a.association_name", "associationName")
+      .addSelect("a.id", "associationId")
+      .addSelect("p.province_name", "proviencName")
+      .addSelect("c.country_name", "countryName")
+      .leftJoinAndSelect(Association, "a", "u.associationId = a.id")
+      .leftJoinAndSelect(Province, "p", "a.provinceId = p.id")
+      .leftJoinAndSelect(Country, "c", "p.countryId = c.id")
       .where(`u.username = '${username}'`)
-      .getOne();
+      .getRawAndEntities();
+      return userData;
   }
 
   async login(loginDto: loginDto){
@@ -47,14 +47,22 @@ export class UsersService {
     if (!userLogin || userLogin.user_password !== loginDto.password) {
       throw new UnauthorizedException();
     }
-    console.log(`userData ${JSON.stringify(userLogin)}`);
-    const userInfo = await this.fetchUserDetails(loginDto.username);
-    console.log(`associationData ${JSON.stringify(userInfo)}`);
+    const userInformation = await this.fetchUserDetails(loginDto.username);
 
     const payload = {
       status: 'success',
       code: '200',
       data: {
+        userInfo: {
+          id: userInformation.raw[0].userId,
+          username: userInformation.raw[0].userName,
+          emailId: userInformation.raw[0].userEmail,
+          association: {
+            id: userInformation.raw[0].associationId,
+            name: userInformation.raw[0].associationName,
+            country: userInformation.raw[0].countryName
+          }
+        }
       } 
     };
     return payload;
