a2342d55bab917be481b41c5cf37cfc9271f9fa7
JayramPai <jayrampai@doshaheen.com>
2023-03-09 11:08:44 +0530

    Adding fix for the CORS issue

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['*'],
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+    allowedHeaders: ['content-type']
+  });
   await app.listen(3000);
 }
 bootstrap();
